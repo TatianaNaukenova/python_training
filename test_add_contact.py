f68069376f87acf76d3d8877979e3d42e787de57
Tatiana Naukenova <petrochenkota@gmail.com>
2020-02-28 13:48:07 +0300

    Parametrs created

--- test_add_contact.py
+++ test_add_contact.py
@@ -17,11 +17,21 @@ class AddContact(unittest.TestCase):
     def test_add_contact(self):
         driver = self.driver
         self.open_homepage(driver)
-        self.login(driver)
+        self.login(driver, username="admin", password="secret")
         self.open_contact_creation_page(driver)
-        self.create_contact(driver)
+        self.create_contact(driver, firstname="Anna", lastname="Smith", mob_phone="+79111024402", email="qwerty@mail.mail", bday="1", bmonth="January", byear="1955")
         self.return_to_homepage(driver)
         self.logout(driver)
+
+    def test_add_empty_contact(self):
+        driver = self.driver
+        self.open_homepage(driver)
+        self.login(driver, username="admin", password="secret")
+        self.open_contact_creation_page(driver)
+        self.create_contact(driver, firstname="", lastname="", mob_phone="", email="", bday="", bmonth="-", byear="")
+        self.return_to_homepage(driver)
+        self.logout(driver)
+
 
     def logout(self, driver):
         driver.find_element_by_link_text("Logout").click()
@@ -29,42 +39,43 @@ class AddContact(unittest.TestCase):
     def return_to_homepage(self, driver):
         driver.find_element_by_link_text("home page").click()
 
-    def create_contact(self, driver):
+    def create_contact(self, driver, firstname, lastname, mob_phone, email, bday, bmonth, byear):
         # fill contact form
         driver.find_element_by_name("firstname").click()
         driver.find_element_by_name("firstname").clear()
-        driver.find_element_by_name("firstname").send_keys("Anna")
+        driver.find_element_by_name("firstname").send_keys(firstname)
         driver.find_element_by_name("lastname").click()
         driver.find_element_by_name("lastname").clear()
-        driver.find_element_by_name("lastname").send_keys("Smith")
+        driver.find_element_by_name("lastname").send_keys(lastname)
         driver.find_element_by_name("mobile").click()
         driver.find_element_by_name("mobile").clear()
-        driver.find_element_by_name("mobile").send_keys("+79111024402")
+        driver.find_element_by_name("mobile").send_keys(mob_phone)
         driver.find_element_by_name("email").click()
         driver.find_element_by_name("email").clear()
-        driver.find_element_by_name("email").send_keys("qwerty@mail.mail")
+        driver.find_element_by_name("email").send_keys(email)
+        # birthday
         driver.find_element_by_name("bday").click()
-        Select(driver.find_element_by_name("bday")).select_by_visible_text("1")
+        Select(driver.find_element_by_name("bday")).select_by_visible_text(bday)
         driver.find_element_by_name("bday").click()
         driver.find_element_by_name("bmonth").click()
-        Select(driver.find_element_by_name("bmonth")).select_by_visible_text("January")
+        Select(driver.find_element_by_name("bmonth")).select_by_visible_text(bmonth)
         driver.find_element_by_name("bmonth").click()
         driver.find_element_by_name("byear").click()
         driver.find_element_by_name("byear").clear()
-        driver.find_element_by_name("byear").send_keys("1955")
+        driver.find_element_by_name("byear").send_keys(byear)
         # submit contact creation
         driver.find_element_by_xpath("(//input[@name='submit'])[2]").click()
 
     def open_contact_creation_page(self, driver):
         driver.find_element_by_link_text("add new").click()
 
-    def login(self, driver):
+    def login(self, driver, username, password):
         driver.find_element_by_name("user").click()
         driver.find_element_by_name("user").clear()
-        driver.find_element_by_name("user").send_keys("admin")
+        driver.find_element_by_name("user").send_keys(username)
         driver.find_element_by_name("pass").click()
         driver.find_element_by_name("pass").clear()
-        driver.find_element_by_name("pass").send_keys("secret")
+        driver.find_element_by_name("pass").send_keys(password)
         driver.find_element_by_xpath("//input[@value='Login']").click()
 
     def open_homepage(self, driver):
