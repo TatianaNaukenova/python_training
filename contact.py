fa36b1fb16334f7cdc37511b520589a87e4184f2
Tatiana Naukenova <petrochenkota@gmail.com>
2020-02-28 14:23:45 +0300

    Added class "Contact"

---
+++ contact.py
@@ -0,0 +1,13 @@
+
+
+class Contact:
+
+    def __init__(self, firstname, lastname, mphone, email, bday, bmonth, byear):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.mphone = mphone
+        self.email = email
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
+
