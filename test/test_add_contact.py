a7ae6fe510da5e7fbc317faea136dbc26090e290
Tatiana Naukenova <petrochenkota@gmail.com>
2020-03-03 18:36:31 +0300

    Login and logout moved to fixture

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,17 +1,12 @@
 # -*- coding: utf-8 -*-
-
 from model.contact import Contact
 
 
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.add_new(Contact(firstname="Anna", lastname="Smith", mphone="+79111024402", email="qwerty@mail.mail", bday="1", bmonth="January", byear="1955"))
-    app.session.logout()
 
 
 def test_add_empty_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.add_new(Contact(firstname="", lastname="", mphone="", email="", bday="", bmonth="-", byear=""))
-    app.session.logout()
 
 
