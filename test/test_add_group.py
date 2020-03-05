e62fcef0c4e515b9d0973d188d5e41f67015c4ec
Tatiana Naukenova <petrochenkota@gmail.com>
2020-03-05 18:55:23 +0300

    assertion: "compare group list length" added

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,8 +3,14 @@ from model.group import Group
 
 
 def test_add_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="new_group", header="qwe", footer="rty"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
 
 def test_add_empty_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="", header="", footer=""))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
