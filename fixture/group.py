27b96c11a87986f2dbf52a10119e799c3900911d
Tatiana Naukenova <petrochenkota@gmail.com>
2020-03-05 12:21:07 +0300

    Checking current page is added to "open_group_page" method

--- fixture/group.py
+++ fixture/group.py
@@ -5,7 +5,8 @@ class GroupHelper:
 
     def open_groups_page(self):
         driver = self.app.driver
-        driver.find_element_by_link_text("groups").click()
+        if not (driver.current_url.endswith("/group.php") and len(driver.find_elements_by_name("new")) > 0):
+            driver.find_element_by_link_text("groups").click()
 
     def create(self, group):
         driver = self.app.driver
@@ -57,3 +58,8 @@ class GroupHelper:
     def select_first_group(self):
         driver = self.app.driver
         driver.find_element_by_name("selected[]").click()
+
+    def count(self):
+        driver = self.app.driver
+        self.open_groups_page()
+        return len(driver.find_elements_by_name("selected[]"))
