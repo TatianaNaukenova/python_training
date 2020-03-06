60b2ef879a62cef546514fafc7fcc0e28b8be3de
Tatiana Naukenova <petrochenkota@gmail.com>
2020-03-06 13:16:26 +0300

    Compare length of groups lists (before and after)

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,10 +3,16 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
+    old_contacts = app.contact.get_contact_list()
     app.contact.add_new(Contact(firstname="Anna", lastname="Smith", mphone="+79111024402", email="qwerty@mail.mail", bday="1", bmonth="January", byear="1955"))
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) + 1 == len(new_contacts)
 
 
 def test_add_empty_contact(app):
+    old_contacts = app.contact.get_contact_list()
     app.contact.add_new(Contact(firstname="", lastname="", mphone="", email="", bday="", bmonth="-", byear=""))
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) + 1 == len(new_contacts)
 
 
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -2,6 +2,9 @@ from model.contact import Contact
 
 
 def test_modify_contact_phone(app):
+    old_contacts = app.contact.get_contact_list()
     if app.contact.count() == 0:
         app.contact.add_new(Contact(firstname="John", lastname="Smith"))
     app.contact.modify_first_contact(Contact(mphone="999-999-999"))
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) == len(new_contacts)
