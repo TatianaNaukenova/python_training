fec7508510634990d9b6782c77772e57f2261739
Tatiana Naukenova <petrochenkota@gmail.com>
2020-03-06 13:15:29 +0300

    Attribute "id" added

--- model/contact.py
+++ model/contact.py
@@ -2,7 +2,7 @@
 
 class Contact:
 
-    def __init__(self, firstname=None, lastname=None, mphone=None, email=None, bday=None, bmonth=None, byear=None):
+    def __init__(self, firstname=None, lastname=None, mphone=None, email=None, bday=None, bmonth=None, byear=None, id=None):
         self.firstname = firstname
         self.lastname = lastname
         self.mphone = mphone
@@ -10,4 +10,5 @@ class Contact:
         self.bday = bday
         self.bmonth = bmonth
         self.byear = byear
+        self.id = id
 
