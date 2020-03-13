1bea287be90f10f1c37f9ce15e42037bf7b6ca76
Tatiana Naukenova <petrochenkota@gmail.com>
2020-03-13 14:51:48 +0300

    Added some methods: __repr__ for representation of lests elements __eq__ for equation and id_or_max as helper for sorting

--- model/contact.py
+++ model/contact.py
@@ -1,4 +1,4 @@
-
+from sys import maxsize
 
 class Contact:
 
@@ -11,4 +11,18 @@ class Contact:
         self.bmonth = bmonth
         self.byear = byear
         self.id = id
+
+    def __repr__(self):
+        return "%s:%s:%s" % (self.id, self.firstname, self.lastname)
+
+    def __eq__(self, other):
+        return (self.id is None or other.id is None or self.id == other.id) and self.firstname == other.firstname and self.lastname == other.lastname
+
+    def id_or_max(self):
+        if self.id:
+            return int(self.id)
+        else:
+            return maxsize
+
+
 
