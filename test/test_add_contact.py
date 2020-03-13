1f9a91151bbe871ee25591874a07f49ddb1638fe
Tatiana Naukenova <petrochenkota@gmail.com>
2020-03-13 14:52:46 +0300

    Tests are modified, added cashing and hashing of lists of contacts

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -2,17 +2,25 @@
 from model.contact import Contact
 
 
+
 def test_add_contact(app):
     old_contacts = app.contact.get_contact_list()
-    app.contact.add_new(Contact(firstname="Anna", lastname="Smith", mphone="+79111024402", email="qwerty@mail.mail", bday="1", bmonth="January", byear="1955"))
+    contact = Contact(firstname="Anna", lastname="Smith", mphone="+79111024402", email="qwerty@mail.mail", bday="1", bmonth="January", byear="1955")
+    app.contact.add_new(contact)
+    assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts) + 1 == len(new_contacts)
+    old_contacts.append(contact)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
-def test_add_empty_contact(app):
-    old_contacts = app.contact.get_contact_list()
-    app.contact.add_new(Contact(firstname="", lastname="", mphone="", email="", bday="", bmonth="-", byear=""))
-    new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts) + 1 == len(new_contacts)
+# def test_add_empty_contact(app):
+#     old_contacts = app.contact.get_contact_list()
+#     contact = Contact(firstname="", lastname="", mphone="", email="", bday="", bmonth="-", byear="")
+#     app.contact.add_new(contact)
+#     new_contacts = app.contact.get_contact_list()
+#     assert len(old_contacts) + 1 == len(new_contacts)
+#     old_contacts.append(contact)
+#     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+
 
 
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -1,10 +1,19 @@
 from model.contact import Contact
+from random import randrange
 
 
 def test_modify_contact_phone(app):
-    old_contacts = app.contact.get_contact_list()
     if app.contact.count() == 0:
         app.contact.add_new(Contact(firstname="John", lastname="Smith"))
-    app.contact.modify_first_contact(Contact(mphone="999-999-999"))
+    old_contacts = app.contact.get_contact_list()
+    # index = randrange(len(old_contacts))
+    index = 1
+    contact = Contact(mphone="999-999-999")
+    contact.id = old_contacts[index].id
+    contact.lastname = old_contacts[index].lastname
+    contact.firstname = old_contacts[index].firstname
+    app.contact.modify_contact_by_index(index, contact)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
+    old_contacts[index] = contact
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
